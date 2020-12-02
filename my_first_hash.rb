5289988882dffd54f1b25a08c12332d067ba1069
Clint Richards <richardsclint17@gmail.com>
2020-12-02 07:02:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,12 +35,11 @@ def adding
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
-                  "muskets" => 2,
-                  "gun powder" => 4
-                 }
+                }
 
+          shipping_manifest["gun powder"] = 4    
+          shipping_manifest["muskets"] = 2             
 
-
-  shipping_manifest
+shipping_manifest
 
 end
