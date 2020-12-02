711e50e2687bfdf56fdf60df0a80c62a6ca27eb5
Clint Richards <richardsclint17@gmail.com>
2020-12-02 06:37:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,14 +38,5 @@ def adding
                   "muskets" => 2
                   "gun powder" => 4
                  }
-                 # add 2 muskets to the shipping_manifest hash below
-                   shipping_manifest["muskets"] = 2
-
-
-                   # add 4 gun powder to the shipping_manifest hash below
-                   shipping_manifest["gun powder"] = 4
-
-
-                   # return the shipping_manifest hash below
-                   shipping_manifest
+    shipping_manifest
 end
