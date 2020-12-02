6713afb1f3195de88952f493b77756b925a8c3c6
Clint Richards <richardsclint17@gmail.com>
2020-12-02 06:49:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,7 +36,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
                   "muskets" => 2,
-                  "gun powder" => 4           
+                  "gun powder" => 4
                  }
 
 shipping_manifest["gun powder"] = 4
